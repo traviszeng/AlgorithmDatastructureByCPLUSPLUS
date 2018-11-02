9036bfc271fe1a24fa793dbdd9d301ef699d46fc
traviszeng <traviszeng7@gmail.com>
2018-11-02 11:51:14 +0800

    add Queue and SeqQueue

---
+++ Data_Structure/Queue/Queue.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef QUEUE_H
+#define QUEUE_H
+
+/*
+队列的抽象类
+**/
+const int maxSize = 50;
+template<class T>
+class Queue {
+	public Queue() {};
+	public ~Queue() {};
+
+	virtual void EnQueue(const T& x) = 0;
+	virtual bool DeQueue(T& x) = 0;
+	virtual bool getFront(T& x) = 0;
+	virtual bool isEmpty()const = 0;
+	virtual bool isFull()const = 0;
+	virtual int getSize()const = 0;
+};
+
+
+
+#endif // !QUEUE_H
