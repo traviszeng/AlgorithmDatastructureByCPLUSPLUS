0577b2f1e3a071ae6268be0fd2d544feede59a8a
traviszeng <traviszeng7@gmail.com>
2018-11-04 23:00:56 +0800

    add Queue

--- Data_Structure/Queue/Queue.h
+++ Data_Structure/Queue/Queue.h
@@ -8,10 +8,10 @@
 const int maxSize = 50;
 template<class T>
 class Queue {
-	public Queue() {};
-	public ~Queue() {};
+	//public Queue() {}
+	//public ~Queue() {}
 
-	virtual void EnQueue(const T& x) = 0;
+	virtual bool EnQueue(const T& x) = 0;
 	virtual bool DeQueue(T& x) = 0;
 	virtual bool getFront(T& x) = 0;
 	virtual bool isEmpty()const = 0;
