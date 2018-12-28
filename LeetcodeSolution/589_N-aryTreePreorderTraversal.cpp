c70437378e8154d60ca8a643db89b9fa47351e7f
traviszeng <traviszeng7@gmail.com>
2018-12-28 17:36:56 +0800

    add 589

---
+++ LeetcodeSolution/589_N-aryTreePreorderTraversal.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+#include<iostream>
+using namespace std;
+// Definition for a Node.
+class Node {
+public:
+	int val;
+	vector<Node*> children;
+
+	Node() {}
+
+	Node(int _val, vector<Node*> _children) {
+		val = _val;
+		children = _children;
+	}
+};
+
+vector<int> preorder(Node* root) {
+
+	vector<int> result;
+	if (root == nullptr)
+		return result;
+
+	result.push_back(root->val);
+
+	for (int i = 0; i < root->children.size(); i++) {
+		vector<int> subresult = preorder(root->children[i]);
+		result.insert(result.end(), subresult.begin(), subresult.end());
+	}
+
+	return result;
+}
+
+
+int main() {
+
+	Node* node = new Node();
+	node->val = 3;
+	Node* node1 = new Node();
+	node1->val = 4;
+	Node* node2 = new Node();
+	node2->val = 5;
+	vector<Node *> children;
+	children.push_back(node1);
+	children.push_back(node2);
+	children.push_back(node);
+	Node *root = new Node(1, children);
+
+	vector<int> traversal = preorder(root);
+	for (int i = 0; i < traversal.size(); i++)
+	{
+		cout << traversal[i] << " ";
+	}
+	system("pause");
+
+}
