a0c4417d40118d1387c62072682ed5924e14ad8b
traviszeng <traviszeng7@gmail.com>
2019-01-04 10:57:20 +0800

    finish problem 19

--- Coding_Interview/19_regexMatch.cpp
+++ Coding_Interview/19_regexMatch.cpp
@@ -12,6 +12,28 @@ bool match(const char* str, const char* pattern)
 }
 
 bool matchCore(const char* str, const char* pattern) {
+	if (*str == '\0'&&*pattern == '\0')
+		return true;
+	if (*str != '\0'&&*pattern == '\0')
+		return false;
+
+	if (*(pattern + 1) == '*') {
+		if (*pattern == *str || (*pattern == '.'&&*str != '\0'))
+			//转到另外的状态
+			return matchCore(str + 1, pattern + 2)
+			//保持现在状态机来匹配
+			|| matchCore(str + 1, pattern)
+			//忽略'*'
+			|| matchCore(str, pattern + 2);
+
+		else
+			//忽略*
+			return matchCore(str, pattern + 2);
+		
+	}
+	if (*str == *pattern || (*pattern == '.'&&*str != '\0'))
+		return matchCore(str + 1, pattern + 1);
+	return false;
 
 }
 
@@ -61,6 +83,6 @@ int main(int argc, char* argv[])
 	Test("Test28", "aaba", "ab*a*c*a", false);
 	Test("Test29", "bbbba", ".*a*a", true);
 	Test("Test30", "bcbbabab", ".*a*a", false);
-
+	system("pause");
 	return 0;
 }
