3889b8f9dc55ef2ddf054eb109a71c11fb78a70b
traviszeng <traviszeng7@gmail.com>
2019-02-05 11:37:14 +0800

    add unique binary tree II

---
+++ LeetcodeSolution/98_uniqueBinarySearchTreeII.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+vector<TreeNode*> generateTrees(int n, int st = 1)
+{
+	vector<TreeNode*> ans;
+	if (n == st)
+	{
+		TreeNode* head = new TreeNode(n);
+		ans.push_back(head);
+		return ans;
+	}
+	for (int i = st; i <= n; i++)
+	{
+		vector<TreeNode*> leftTree;
+		vector<TreeNode*> rightTree;
+		if (i>st)leftTree = generateTrees(i - 1, st);
+		if (i<n)rightTree = generateTrees(n, i + 1);
+		for (int x = 0; x<leftTree.size(); x++)
+		{
+			for (int y = 0; y<rightTree.size(); y++)
+			{
+				TreeNode* head = new TreeNode(i);
+				head->left = leftTree[x];
+				head->right = rightTree[y];
+				ans.push_back(head);
+			}
+		}
+		for (int x = 0; x<leftTree.size() && rightTree.size() == 0; x++)
+		{
+			TreeNode* head = new TreeNode(i);
+			head->left = leftTree[x];
+			ans.push_back(head);
+		}
+		for (int y = 0; y<rightTree.size() && leftTree.size() == 0; y++)
+		{
+			TreeNode* head = new TreeNode(i);
+			head->right = rightTree[y];
+			ans.push_back(head);
+		}
+	}
+	return ans;
+}
+
+
+int main() {
+	generateTrees(3);
+
+}
