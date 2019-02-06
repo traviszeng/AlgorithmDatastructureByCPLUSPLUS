b971258f1997a68f4ed2476789bdee5cd4003809
traviszeng <traviszeng7@gmail.com>
2019-02-06 12:41:41 +0800

    finish 114 flatten binary tree

---
+++ LeetcodeSolution/114_flattenBinaryTree.cpp
@@ -0,0 +1,22 @@
+/**
+* Definition for a binary tree node.
+*
+*/
+#include<iostream>
+#include<queue>
+using namespace std;
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+void flatten(TreeNode* root) {
+
+}
+
+void append(TreeNode* &root) {
+	
+}
