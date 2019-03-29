d68078c8ba6697ad0d6c6ba9b34c02590671d748
traviszeng <traviszeng7@gmail.com>
2019-03-29 11:08:13 +0800

    coding interview 34 find path in tree

---
+++ Coding_Interview/34_findPathInTree.cpp
@@ -0,0 +1,184 @@
+#include<iostream>
+#include "BinaryTree.h"
+#include<stack>
+#include<vector>
+using namespace std;
+void printPath(vector<int> vec) {
+	for (auto i = vec.begin(); i != vec.end(); i++) {
+		cout << *i << "\t";
+	}
+	cout << endl;
+}
+
+void findPathHelper(BinaryTreeNode *root,vector<int>& vec, int expectedSum,int currentSum) {
+	currentSum += root->m_nValue;
+	vec.push_back(root->m_nValue);
+
+	if (currentSum == expectedSum && (root->m_pLeft == nullptr&&root->m_pRight == nullptr)) {
+		printPath(vec);
+	}
+
+	if (root->m_pLeft != nullptr)
+		findPathHelper(root->m_pLeft, vec, expectedSum, currentSum);
+	if (root->m_pRight != nullptr)
+		findPathHelper(root->m_pRight, vec, expectedSum, currentSum);
+
+
+	vec.pop_back();
+}
+
+
+
+void FindPath(BinaryTreeNode* pRoot, int expectedSum)
+{
+	if (pRoot == nullptr)
+		return;
+	vector<int> nodeVec;
+	findPathHelper(pRoot, nodeVec, expectedSum, 0);
+}
+
+
+// ====================测试代码====================
+void Test(char* testName, BinaryTreeNode* pRoot, int expectedSum)
+{
+	if (testName != nullptr)
+		printf("%s begins:\n", testName);
+
+	FindPath(pRoot, expectedSum);
+
+	printf("\n");
+}
+
+//            10
+//         /      \
+//        5        12
+//       /\        
+//      4  7     
+// 有两条路径上的结点和为22
+void Test1()
+{
+	BinaryTreeNode* pNode10 = CreateBinaryTreeNode(10);
+	BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+	BinaryTreeNode* pNode12 = CreateBinaryTreeNode(12);
+	BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+	BinaryTreeNode* pNode7 = CreateBinaryTreeNode(7);
+
+	ConnectTreeNodes(pNode10, pNode5, pNode12);
+	ConnectTreeNodes(pNode5, pNode4, pNode7);
+
+	printf("Two paths should be found in Test1.\n");
+	Test("Test1", pNode10, 22);
+
+	DestroyTree(pNode10);
+}
+
+//            10
+//         /      \
+//        5        12
+//       /\        
+//      4  7     
+// 没有路径上的结点和为15
+void Test2()
+{
+	BinaryTreeNode* pNode10 = CreateBinaryTreeNode(10);
+	BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+	BinaryTreeNode* pNode12 = CreateBinaryTreeNode(12);
+	BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+	BinaryTreeNode* pNode7 = CreateBinaryTreeNode(7);
+
+	ConnectTreeNodes(pNode10, pNode5, pNode12);
+	ConnectTreeNodes(pNode5, pNode4, pNode7);
+
+	printf("No paths should be found in Test2.\n");
+	Test("Test2", pNode10, 15);
+
+	DestroyTree(pNode10);
+}
+
+//               5
+//              /
+//             4
+//            /
+//           3
+//          /
+//         2
+//        /
+//       1
+// 有一条路径上面的结点和为15
+void Test3()
+{
+	BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+	BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+	BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+	BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+	BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+
+	ConnectTreeNodes(pNode5, pNode4, nullptr);
+	ConnectTreeNodes(pNode4, pNode3, nullptr);
+	ConnectTreeNodes(pNode3, pNode2, nullptr);
+	ConnectTreeNodes(pNode2, pNode1, nullptr);
+
+	printf("One path should be found in Test3.\n");
+	Test("Test3", pNode5, 15);
+
+	DestroyTree(pNode5);
+}
+
+// 1
+//  \
+//   2
+//    \
+//     3
+//      \
+//       4
+//        \
+//         5
+// 没有路径上面的结点和为16
+void Test4()
+{
+	BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+	BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+	BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+	BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+	BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+
+	ConnectTreeNodes(pNode1, nullptr, pNode2);
+	ConnectTreeNodes(pNode2, nullptr, pNode3);
+	ConnectTreeNodes(pNode3, nullptr, pNode4);
+	ConnectTreeNodes(pNode4, nullptr, pNode5);
+
+	printf("No paths should be found in Test4.\n");
+	Test("Test4", pNode1, 16);
+
+	DestroyTree(pNode1);
+}
+
+// 树中只有1个结点
+void Test5()
+{
+	BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+
+	printf("One path should be found in Test5.\n");
+	Test("Test5", pNode1, 1);
+
+	DestroyTree(pNode1);
+}
+
+// 树中没有结点
+void Test6()
+{
+	printf("No paths should be found in Test6.\n");
+	Test("Test6", nullptr, 0);
+}
+
+int main(int argc, char* argv[])
+{
+	Test1();
+	Test2();
+	Test3();
+	Test4();
+	Test5();
+	Test6();
+	system("pause");
+	return 0;
+}
