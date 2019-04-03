61cdb2d32f37a3215e58d5137a513458e6ac6ef1
traviszeng <traviszeng7@gmail.com>
2019-04-03 21:00:35 +0800

    add solution for PDD problem 4

--- Interview Code/PDD TEST/pdd4.cpp
+++ Interview Code/PDD TEST/pdd4.cpp
@@ -20,3 +20,41 @@ execution
 sample output:
 5
 */
+
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+
+
+int minDistance(string word1, string word2) {
+	int len1 = word1.length();
+	int len2 = word2.length();
+
+	vector<vector<int>>dp(len1 + 1, vector<int>(len2 + 1));
+	for (int i = 0; i <= len1; i++) {
+		for (int j = 0; j <= len2; j++) {
+			if (i == 0) {
+				dp[i][j] = j;   //如果 i=0 则需要插入j个字符
+			}
+			else if (j == 0) {
+				dp[i][j] = i;    //如果j=0则需要删除i个字符
+			}
+			else if (word1[i - 1] == word2[j - 1]) {   //如果字符相同则不做变换，等于这个字符之前的步数
+				dp[i][j] = dp[i - 1][j - 1];
+			}
+			else {                          //否则 就从插入，删除，修改中选择最小的 加 1 
+				dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+			}
+		}
+	}
+	return dp[len1][len2];
+}
+
+int main() {
+	string a, b;
+	cin >> a >> b;
+	cout<<minDistance(a, b);
+	system("pause");
+}
